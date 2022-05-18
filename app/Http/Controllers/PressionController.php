2057f6f26d05e53dfaef9df20f1d0b779f1567c2
BEGUE <beguethomas439@gmail.com>
2022-05-18 11:44:33 +0400

    Pression controller ajouter

---
+++ app/Http/Controllers/PressionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PressionController extends Controller
+{
+    public function pressionpneu(){
+        return view('pressionpneu');
+    }
+}
